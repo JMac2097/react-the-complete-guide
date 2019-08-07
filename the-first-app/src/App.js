61053cfcd5d505cda3c128bb8a0f494b4d955c4e
John McKie <john.mckie@remarkable.net>
2019-08-07 16:04:03 +0100

    Even more changes

--- the-first-app/src/App.js
+++ the-first-app/src/App.js
@@ -12,6 +12,26 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    // console.log('hello, there has been a click');
+    // DOn't do this this.state.persons[0].name = 'Freddddd';
+    this.setState({    
+      persons: [
+      { name: 'Derek', age: 89 },
+      { name: 'Arnold', age: 98 },
+      { name: 'Yoda', age: 38 },
+    ] })
+  }
+
+  nameChangedHandler  = (event) => {
+    this.setState({    
+      persons: [
+      { name: 'Derek', age: 89 },
+      { name: event.target.value, age: 98 },
+      { name: 'Yoda', age: 38 },
+    ] })
+  }
+
   render() {
     return (
       <div className="App">
@@ -19,10 +39,18 @@ class App extends Component {
       
         <h1>Hi, I am a react app</h1>
         <p>This is paragraph 2!!</p>
-        <button>Switch Name</button>
-        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
-        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age} />
-        <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}> A jedi master </Person>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person 
+        name = {this.state.persons[0].name} 
+        age = {this.state.persons[0].age} />
+        <Person 
+        name = {this.state.persons[1].name} 
+        age = {this.state.persons[1].age}
+        click={this.switchNameHandler}
+        changed={this.nameChangedHandler} />
+        <Person 
+        name = {this.state.persons[2].name} 
+        age = {this.state.persons[2].age}> A jedi master </Person>
 
  
       </div>
