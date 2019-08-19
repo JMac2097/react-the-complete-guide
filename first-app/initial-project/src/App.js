c62444ad5a261b83c7cfba30e5d845e643dd6b54
John McKie <john.mckie@remarkable.net>
2019-08-19 11:44:38 +0100

    setup of assignment-1 project

--- first-app/initial-project/src/App.js
+++ first-app/initial-project/src/App.js
@@ -21,11 +21,31 @@ class App extends Component {
    } )
   }
 
+  nameChangedHandler = (event) => {
+    this.setState( {people: [
+      { name: 'Derek', age: 356 },
+      { name: event.target.value, age: 58 },
+      { name: 'Dick', age: 23213 }
+    ]
+   } )
+  }
+
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
+
     return (
       <div className="App">
         <h1> Hello world</h1>
-        <button onClick={this.switchNameHandler.bind(this, 'Timothy!')}>Switch Name</button>
+
+        <button style={style} onClick={this.switchNameHandler.bind(this, 'Derp!')}>Switch Name</button>
         <Person 
         name={this.state.people[0].name}
         age={this.state.people[0].age}/>
@@ -33,6 +53,7 @@ class App extends Component {
         name={this.state.people[1].name} 
         age={this.state.people[1].age}
         click={this.switchNameHandler.bind(this, 'Gerald!')}
+        changed={this.nameChangedHandler}
         >My hobbies: killing</Person>
         <Person 
         name={this.state.people[2].name} 
