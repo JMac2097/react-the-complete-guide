33c0bbb4c234d1034f41c133c1d93c1b7ae5d84f
John McKie <john.mckie@remarkable.net>
2019-08-20 16:39:45 +0100

    setup of of the redux assignment app

---
+++ assignment-1-redux/src/UserOutput/UserOutput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const UserOutput = () => {
+
+    return (
+
+        <div>
+            <p>More test text</p>
+            <p>More test text</p>
+        </div>
+    )
+}
+
+export default UserOutput;
